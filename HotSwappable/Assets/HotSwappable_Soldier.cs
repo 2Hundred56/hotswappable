cd548f026699ef77ddc0b3406d122e1ba76572ea
Ethan Saff <ethan.saff@gmail.com>
2016-08-14 16:51:25 -0400

    Getting Set Up

---
+++ HotSwappable/Assets/HotSwappable_Soldier.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotSwappable_Soldier : HotSwappable {
+
+	// Use this for initialization
+	void Start () {
+		Controlled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+		
+		
+}
