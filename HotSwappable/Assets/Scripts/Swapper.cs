cdae002851e9ce57250796a3dcecdd12789cf3dc
Ethan Saff <ethan.saff@gmail.com>
2016-08-17 20:00:36 -0400

    Click-to-swap: Attempt 1

--- HotSwappable/Assets/Scripts/Swapper.cs
+++ HotSwappable/Assets/Scripts/Swapper.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityStandardAssets.Cameras;
 public class Swapper : MonoBehaviour {
-
+	float SwapRange = 1000;
 	// Use this for initialization
 	void Start () {
 
@@ -48,6 +48,28 @@ public class Swapper : MonoBehaviour {
 
 
 
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			Plane p = new Plane (Camera.main.transform.forward , transform.position);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 100)) {
+				GameObject closest = hit.collider.gameObject;
+				if (Vector3.Distance (transform.position, closest.transform.position) < SwapRange) {
+					Camera.main.transform.parent = closest.transform;
+					Camera.main.transform.localRotation = Quaternion.Euler (new Vector3 (35.2904f, 0, 0));
+					Camera.main.transform.localPosition = new Vector3 (0, 9.8f, -(closest.GetComponent<Collider> ().bounds.size.magnitude + 8.63f)) / 2f;
+					closest.AddComponent<FreeLookCam> ();
+					closest.AddComponent<Swapper> ();
+					closest.AddComponent<Swappable_Controller> ();
+					closest.GetComponent<HotSwappable> ().Controlled = true;
+					Destroy (GetComponent<FreeLookCam> ());
+					Destroy (GetComponent<Swapper> ());
+					Destroy (GetComponent<Swappable_Controller> ());
+					GetComponent<HotSwappable> ().Controlled = false;
+				}
+
+			}
 		}
 	
 	}
