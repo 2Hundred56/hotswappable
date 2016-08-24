8584ca7f42f6349f14d9eab52ae6af6e81841a56
Ethan Saff <ethan.saff@gmail.com>
2016-08-23 20:22:50 -0400

    A weighty commit

--- HotSwappable/Assets/Scripts/Swappables/HotSwappable_Launchable.cs
+++ HotSwappable/Assets/Scripts/Swappables/HotSwappable_Launchable.cs
@@ -5,9 +5,13 @@ public class HotSwappable_Launchable : HotSwappable {
 
 	float throw_cooldown = 100f;
 	float throw_state=0;
+	float throw_strength=0;
 
 	public void Update () {
-		throw_cooldown = GetComponent<Rigidbody> ().mass * 10;
+		throw_strength = 50 / GetComponent<Rigidbody> ().mass;
+		if (throw_strength > 7) {
+			throw_strength = 7;
+		}
 		if (throw_state > 0) {
 			throw_state--;
 		}
@@ -15,7 +19,7 @@ public class HotSwappable_Launchable : HotSwappable {
 
 	public override void Launch() {
 		if (throw_state==0) {
-			GetComponent<Rigidbody> ().velocity += transform.up * 5 + transform.forward * 5;
+			GetComponent<Rigidbody> ().velocity += transform.up * throw_strength + transform.forward * throw_strength;
 			throw_state = throw_cooldown;
 		}
 
